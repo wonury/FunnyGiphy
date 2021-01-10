5c3b506c5d70e081949322d0bd340aed3d7f3adb
wonury <nspred@gmail.com>
2021-01-10 15:39:58 +0900

    프로젝트 셋업

---
+++ FunnyGiphy/Sources/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  FunnyGiphy
+//
+//  Created by Lee Won-woo on 2021/01/10.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let rect = UIScreen.main.bounds
+        let window = UIWindow(frame: CGRect(x: 0, y: 0, width: rect.width, height: rect.height))
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        return true
+    }
+}
